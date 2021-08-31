378be32cdc219a288ce71706585f9d8c16d1ec01
Yousong <dbthd0220@naver.com>
2021-09-01 00:01:35 +0900

    re-add settings

---
+++ nuxt-typescript-sample/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <div>
+      <logo />
+      <h1 class="title">{{ authorName }}</h1>
+      // This is the sample
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from "nuxt-property-decorator";
+import Logo from "~/components/VuetifyLogo.vue";
+
+@Component({
+  components: {
+    Logo
+  }
+})
+export default class extends Vue {
+  authorName: string = "Usong";
+}
+</script>
